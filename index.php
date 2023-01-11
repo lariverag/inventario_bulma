4336d7460c29cd29ec39a011be6402cb7e2d460c
Alfredo Rivera <lariveragut@unadvirtual.edu.co>
2023-01-11 15:49:42 -0500

    Primer commit cargando y creando el head y el navbar

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+        include "./inc/head.php";
+    ?>
+</head>
+<body>
+    <?php
+        include "./inc/navbar.php";
+    ?>
+</body>
+</html>
