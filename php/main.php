2c0f7d1ee71774d414e90cb710d3461af41c9ff4
Alfredo Rivera <lariveragut@unadvirtual.edu.co>
2023-01-13 13:17:47 -0500

    se actauliza el proyecto con los ultimos cambios hasta el video 17

--- php/main.php
+++ php/main.php
@@ -4,6 +4,12 @@
         $pdo = new PDO('mysql:host=localhost;dbname=inventario','root','');
         return $pdo;
     }
+    
+    # Prueba de insercion a la base de datos tabla categoria#
+
+    /* $pdo = new PDO('mysql:host=localhost;dbname=inventario','root','');
+    $pdo->query("INSERT INTO categoria(categoria_nombre, categoria_ubicacion) VALUES ('prueba', 'texto ubicacion')");*/
+
 
     # Funcion para Verificar datos #
 
@@ -47,4 +53,76 @@
         return $cadena;
     }
 
+    # Funcion para renombrar fotos #
+
+    function renombrar_fotos($nombre){
+        $nombre = str_ireplace(" ","_",$nombre);
+        $nombre = str_ireplace("/","_",$nombre);
+        $nombre = str_ireplace("#","_",$nombre);
+        $nombre = str_ireplace("-","_",$nombre);
+        $nombre = str_ireplace("$","_",$nombre);
+        $nombre = str_ireplace(".","_",$nombre);
+        $nombre = str_ireplace(",","_",$nombre);
+        $nombre=$nombre."_".rand(0,100);
+        return $nombre;
+
+    }
+
+    # Probar la funcion para evitar que no hayan conflictos con los nombres de las fotos #
+
+    /*$foto="Play Station 5 black/edition";
+    echo renombrar_fotos($foto);*/
+
+    # Funcion paginador de Tablas #
+
+    function paginador_tablas($pagina, $nPaginas, $url, $botones){
+        $tabla='<nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination">';
+
+        if ($pagina<=1) {
+            $tabla.='
+            <a class="pagination-previous" disabled>Anterior</a>
+            <ul class="pagination-list">
+            ';
+        } else {
+            $tabla.='
+            <a class="pagination-previous" href="'.$url.($pagina-1).'">Anterior</a>
+            <ul class="pagination-list">
+                <li><a class="pagination-link" href="'.$url.'1">1</a></li>
+                <li><span class="pagination-ellipsis">&hellip;</span></li>
+            ';
+        }
+
+        $ci=0;
+
+        for ($i=$pagina; $i<=$nPaginas; $i++){
+            if ($ci<=$botones) {
+                break;
+            }
+
+            if ($pagina==$i) {
+                $tabla.='<li><a class="pagination-link is-current" href="'.$url.$i'">'.$i.'</a></li>';
+            } else {
+                $tabla.='<li><a class="pagination-link" href="'.$url.$i'">'.$i.'</a></li>';
+            }
+            $ci++;
+        }     
+
+        if ($pagina==$nPaginas) {
+            $tabla.='
+            </ul>
+            <a class="pagination-next is-disabled" disabled>Siguiente</a>
+            ';
+        } else {
+            $tabla.='
+                <li><span class="pagination-ellipsis">&hellip;</span></li>
+                <li><a class="pagination-link" href="'.$url.$nPaginas.'">'.$nPaginas.'</a></li>
+            </ul>
+            <a class="pagination-next" href="'.$url.($pagina+1).'">Siguiente</a>
+            ';
+        }
+        
+        $tabla.='</nav>';
+        return $tabla;
+
+    }
 ?>
