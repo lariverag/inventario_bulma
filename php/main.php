2160da16ba0d0b2b455d09b18e8c6333c95c37d3
Alfredo Rivera <lariveragut@unadvirtual.edu.co>
2023-01-12 23:00:47 -0500

    Se modifico de acuerdo al video 14-15-16

--- php/main.php
+++ php/main.php
@@ -75,8 +75,54 @@
 
     # Funcion paginador de Tablas #
 
-    function paginador_tablas($pagina, $nPaginas, $url){
+    function paginador_tablas($pagina, $nPaginas, $url, $botones){
+        $tabla='<nav class="pagination is-centered is-rounded" role="navigation" aria-label="pagination">';
+
+        if ($pagina<=1) {
+            $tabla.='
+            <a class="pagination-previous" disabled>Anterior</a>
+            <ul class="pagination-list">
+            ';
+        } else {
+            $tabla.='
+            <a class="pagination-previous" href="'.$url.($pagina-1).'">Anterior</a>
+            <ul class="pagination-list">
+                <li><a class="pagination-link" href="'.$url.'1">1</a></li>
+                <li><span class="pagination-ellipsis">&hellip;</span></li>
+            ';
+        }
+
+        $ci=0;
+
+        for ($i=$pagina; $i<=$nPaginas; $i++){
+            if ($ci<=$botones) {
+                break;
+            }
+
+            if ($pagina==$i) {
+                $tabla.='<li><a class="pagination-link is-current" href="'.$url.$i'">'.$i.'</a></li>';
+            } else {
+                $tabla.='<li><a class="pagination-link" href="'.$url.$i'">'.$i.'</a></li>';
+            }
+            $ci++;
+        }     
+
+        if ($pagina==$nPaginas) {
+            $tabla.='
+            </ul>
+            <a class="pagination-next is-disabled" disabled>Siguiente</a>
+            ';
+        } else {
+            $tabla.='
+                <li><span class="pagination-ellipsis">&hellip;</span></li>
+                <li><a class="pagination-link" href="'.$url.$nPaginas.'">'.$nPaginas.'</a></li>
+            </ul>
+            <a class="pagination-next" href="'.$url.($pagina+1).'">Siguiente</a>
+            ';
+        }
         
+        $tabla.='</nav>';
+        return $tabla;
 
     }
 ?>
