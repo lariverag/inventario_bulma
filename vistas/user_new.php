c7fc0f25942179a0aaf648490b9cbfa775a11f7b
Alfredo Rivera <lariveragut@unadvirtual.edu.co>
2023-01-13 17:03:52 -0500

    se mejora la vista de user_new

--- vistas/user_new.php
+++ vistas/user_new.php
@@ -1,1 +1,37 @@
-Nuevo Usuario
+<div class="container is-fluid mb-6">
+    <h1 class="title">Usuarios</h1>
+    <h2 class="subtitle">Nuevo Usuario</h2>
+</div>
+<div class="container pb-6 pt-6">
+    <div class="form-rest mb-6 mt-6"></div>
+    <form action="" method="" autocomplete="off">
+        <div class="colomns">
+            <div class="column">
+                <div class="control">
+                    <label>Nombre</label>
+                    <input class="input" type="text" name="usuario_nombre" pattern="[a-zAáéíóú´´AÉÍÓÚñÑ ]{.,40}" maxlength="40">
+                </div>
+            </div>
+            <div class="column">
+                <div class="control">
+                    <label>Apellidos</label>
+                    <input class="input" type="text" name="usuario_apellido" pattern="[a-zAáéíóú´´AÉÍÓÚñÑ ]{.,40}" maxlength="40">
+                </div>
+            </div>
+            <div class="column">
+                <div class="control">
+                    <label>Usuario</label>
+                    <input class="input" type="text" name="usuario_usuario" pattern="[a-zAáéíóú´´AÉÍÓÚñÑ ]{.,40}" maxlength="40">
+                </div>
+            </div>
+            <div class="column">
+                <div class="control">
+                    <label>Email</label>
+                    <input class="input" type="email" name="usuario_email" pattern="[a-zAáéíóú´´AÉÍÓÚñÑ ]{.,40}" maxlength="70">
+                </div>
+            </div>
+
+
+        </div>
+    </form>
+</div>
