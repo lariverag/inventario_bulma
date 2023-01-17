8366e46146fd7f3def22a19eb648e91823c40541
Alfredo Rivera <lariveragut@unadvirtual.edu.co>
2023-01-16 21:36:07 -0500

    Se creo el nuevo archivo de buscador de acuerdo al video 28

--- vistas/user_search.php
+++ vistas/user_search.php
@@ -5,6 +5,11 @@
 <div class="container pb-6 pt-6">
     <?php
         require_once "./php/main.php";
+
+        if (isset($_POST['modulo_buscador'])) {
+            require_once "./php/buscador.php";
+        }
+
         if (!isset($_SESSION['busqueda_usuario']) && empty($_SESSION['busqueda_usuario'])) {
         
     ?>
@@ -14,7 +19,7 @@
                 <input type="hidden" name="modulo_buscador" value="usuario">
                 <div class="field is-grouped">
                     <p class="control is-expanded">
-                        <input class="input is-rounded" type="text" name="txt_buscador" placeholder="¿Que estas Buscando" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]{1,30}" maxlength="30">
+                        <input class="input is-rounded" type="text" name="txt_buscador" placeholder="¿Que estas Buscando?" pattern="[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ ]{1,30}" maxlength="30">
                     </p>
                     <p class="control">
                         <button class="button is-info" type="submit">Buscar</button>
