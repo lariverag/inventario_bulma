09aa24df21fa0c5864ae8c2dfebb728a6f61eacd
Alfredo Rivera <lariveragut@unadvirtual.edu.co>
2023-01-17 22:31:28 -0500

    En user_search se llamo el codigo php de usuario_eliminar.php y se valido que la variable user_id_del viniera definida

--- vistas/user_search.php
+++ vistas/user_search.php
@@ -43,7 +43,12 @@
         </div>
     </div>
 
-    <?php 
+    <?php
+
+        if (isset($_GET['user_id_del'])) {
+            require_once "./php/usuario_eliminar.php";
+        }
+
         if (!isset($_GET['page'])) {
             $pagina=1;
         } else {
